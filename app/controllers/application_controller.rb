ce44de79aa814d140cd8506a3f4e6f1a850aab0f
John Tuttle <thejwt@gmail.com>
2017-11-04 18:18:16 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,8 @@ class ApplicationController < Sinatra::Base
   patch '/recipes/:id' do
     @recipe = Recipe.find_by(id: params[:id])
     @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
   end
 
   get '/recipes/new' do
